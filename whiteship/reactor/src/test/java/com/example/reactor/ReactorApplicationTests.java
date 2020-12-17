f301913fb8a0f07c745a15c3fe666a753a17ae66
korea8378 <korea8378@naver.com>
2020-12-17 15:19:23 +0900

    백기선님 유튜브 강의 실습 - StepVerifier

--- whiteship/reactor/src/test/java/com/example/reactor/ReactorApplicationTests.java
+++ whiteship/reactor/src/test/java/com/example/reactor/ReactorApplicationTests.java
@@ -1,13 +1,55 @@
 package com.example.reactor;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+import java.time.Duration;
+
 class ReactorApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+		Flux<String> flux = Flux.just("foo", "bar");
+
+		StepVerifier.create(flux)
+				.expectNext("foo")
+				.expectNext("bar")
+				.verifyComplete();
+
+		StepVerifier.create(flux)
+				.expectNext("foo")
+				.expectNext("bar")
+				.verifyError(RuntimeException.class);
+
+		Flux<User> fluxs = Flux.just(new User("swhite"), new User("jpinkman"));
+
+		StepVerifier.create(fluxs)
+				.assertNext(u -> Assertions.assertThat(u.getUsername()).isEqualTo("swhite"))
+				.assertNext(u -> Assertions.assertThat(u.getUsername()).isEqualTo("jpinkman"))
+				.verifyComplete();
+
+		Flux<Long> intervalFlux = Flux.interval(Duration.ofMillis(100))
+				.take(10);
+
+		StepVerifier.create(intervalFlux)
+				.expectNextCount(10)
+				.verifyComplete();
+
+
 	}
 
+	public static class User {
+		private String username;
+
+		public User(String username) {
+			this.username = username;
+		}
+
+		public String getUsername() {
+			return username;
+		}
+	}
 }
