19300abf3b31d177a8061c554d2001d03b19b2c4
korea8378 <korea8378@naver.com>
2020-12-22 17:20:56 +0900

    토비님 유튜브 강의 실습 - Flux의 특징과 활용방법

--- toby/reactive_streams/src/main/java/com/practice/webflux/contoller/WebFluxController.java
+++ toby/reactive_streams/src/main/java/com/practice/webflux/contoller/WebFluxController.java
@@ -1,9 +1,20 @@
 package com.practice.webflux.contoller;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Slf4j
 @RestController
@@ -21,8 +32,63 @@ public class WebFluxController {
         return m; // subscribe
     }
 
+    @GetMapping("event/{id}")
+    Mono<List<Event>> event(@PathVariable long id) {
+        List<Event> list = Arrays.asList(new Event(1, "event" + 1), new Event(2, "event" + 2));
+        return Mono.just(list);
+    }
+
+    @GetMapping(value = "events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Event> events() {
+//        return Flux
+//                .fromStream(Stream.generate(() -> new Event(System.currentTimeMillis(), "value")))
+//                .delayElements(Duration.ofSeconds(1))
+//                .take(10);
+//        return Flux
+//                .<Event>generate(sink -> sink.next(new Event(System.currentTimeMillis(), "value")))
+//                .delayElements(Duration.ofSeconds(1))
+//                .take(10);
+//        return Flux
+//                .<Event, Long>generate(() -> 1L, (id, sink) -> {
+//                    sink.next(new Event(id, "value" + id));
+//                    return id + 1;
+//                })
+//                .delayElements(Duration.ofSeconds(1))
+////                .take(10);
+//             Flux<Event> eventFlux = Flux
+//                .<Event, Long>generate(() -> 1L, (id, sink) -> {
+//                    sink.next(new Event(id, "value" + id));
+//                    return id + 1;
+//                })
+//                .take(10);
+//
+//             Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1));
+//
+//             return Flux.zip(eventFlux, intervalFlux).map(Tuple2::getT1);
+
+
+        Flux<String> eventFlux = Flux
+                .generate(sink -> sink.next("Value"));
+
+        Flux<Long> intervalFlux = Flux.interval(Duration.ofSeconds(1));
+
+        return Flux
+                .zip(eventFlux, intervalFlux)
+                .map(tu -> new Event(tu.getT2(), tu.getT1()))
+                .take(10);
+
+    }
+
     private String generateHello() {
         log.info("method generateHello()");
         return "Hello Mono";
+    }
+
+    @AllArgsConstructor
+    @Data
+    public static class Event {
+        long id;
+        String value;
+
     }
 }
